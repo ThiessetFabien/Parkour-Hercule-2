a983ec822b26ba6abbdb72532275cbcde13f35df
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-01-15 09:12:06 +0100

    feet/entity hercule

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,17 @@
 console.log('test 1...2...1...2');
+
+// représenter l'entité Hercule
+const Hercule = {
+    // name : Hercule
+    name: 'Hercule',
+    // job : Demi-dieu
+    job: 'Demi-dieu',
+    // age : 35ans
+    age: 35,
+    // department : 75
+    department: 75,
+    // arm : 60.5
+    arm: 60.5,
+    // inRelationship : Yes
+    inRelationship: 'Yes',
+}
